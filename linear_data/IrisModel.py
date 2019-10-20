2adf3ea366a1bf79cb296b850ca960dd362f6b4a
BrandonKates <bjk224@cornell.edu>
2019-10-19 22:47:31 -0400

    added iris

---
+++ linear_data/IrisModel.py
@@ -0,0 +1,94 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import os
+from StochasticBinaryLayer import StochasticBinaryLayer
+
+class IrisModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(IrisModel, self).__init__()
+        self.layer1 = StochasticBinaryLayer(input_size, hidden_size)
+        self.layer2 = StochasticBinaryLayer(hidden_size, num_classes)
+        
+    def forward(self, x, with_grad=True):
+        x = self.layer1(x, with_grad)
+        x = self.layer2(x, with_grad)
+        return x
+
+    def get_grad(self, loss):
+        self.layer1.get_grad(loss)
+        self.layer2.get_grad(loss)
+    
+    def predict(self,x):
+        x = torch.from_numpy(x).type(torch.FloatTensor)
+        #Apply softmax to output.
+        pred = F.softmax(self.forward(x), dim=1)
+
+        ans = []
+        for prediction in pred:
+            ans.append(prediction.argmax().item())
+        return ans
+ 
+def run_model(hidden_size=32, num_epochs=100, batch_size=1, learning_rate=0.001, train_loader = None, test_loader = None):
+    num_classes = 3
+    input_size = 4
+
+    model = IrisModel(input_size, hidden_size, num_classes).cuda()
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+
+    # Train the model
+    total_step = len(train_loader)
+    for epoch in range(num_epochs):
+        for i, batch in enumerate(train_loader):
+            optimizer.zero_grad()
+            # Move tensors to the configured device
+            inputs = batch['input'].float().cuda()
+            labels = batch['label'].cuda()
+            # Forward pass
+            outputs = model(inputs)
+            # One hot encoding buffer that you create out of the loop and just keep reusing
+            #labels_onehot = torch.FloatTensor(batch_size, num_classes)
+
+            # In your for loop
+            #labels_onehot.zero_()
+            #labels_onehot.scatter_(1, (labels.long()).view(-1,1), 1)
+
+            #loss = torch.sum((outputs - labels_onehot.cuda())**2) / batch_size
+            print("Inputs: ", inputs)
+            print("Labels: ", labels)
+            print("Outputs: ", outputs)
+            print()
+            loss = criterion(outputs, labels).cuda()
+            # Backward and optimize
+            model.get_grad(loss)
+            optimizer.step()
+
+            if (i+1) % 10 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                       .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+    # Test the model
+    correct = 0
+    total = 0
+    for batch in test_loader:
+        inputs = batch['input'].float().cuda()
+        labels = batch['label'].cuda()
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on linearly separable data: {} %'.format(100 * correct / total))
+
+    # Save the model checkpoint
+    print(model.state_dict())
+    torch.save(model.state_dict(), 'model.ckpt')
+    print("Model saved to: ", os.getcwd() + "/model.ckpt")
+
+if __name__ == "__main__":
+    from load_iris import getIrisDataLoader
+
+    trainData, testData, train, test = getIrisDataLoader()
---
+++ linear_data/load_iris.py
@@ -0,0 +1,56 @@
+import numpy as np
+import os
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from torch.utils.data.sampler import SubsetRandomSampler
+from sklearn import datasets
+
+
+class IrisDataset(Dataset):
+    """Linearly Separable Dataset."""
+
+    def __init__(self, dataset):
+        self.inputs = dataset['inputs']
+        self.labels = dataset['labels']
+        self.length = len(self.labels)
+    
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        sample = {'input': self.inputs[idx], 'label': self.labels[idx]}
+        return sample
+    
+def getIrisDataLoader(test_split = 0.2, batch_size = 1, num_workers = 1):
+    dataset = datasets.load_iris()
+    inputs = dataset.data
+    labels = dataset.target
+    shuffle_dataset = True
+    random_seed = 42
+
+    # Creating data indices for training and validation splits:
+    indices = list(range(len(labels)))
+    split = int(np.floor(test_split * len(labels)))
+    if shuffle_dataset:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_indices, test_indices = indices[split:], indices[:split]
+
+    # Creating PT data samplers and loaders:
+    train = IrisDataset({'inputs': inputs[train_indices],
+             'labels': labels[train_indices]})
+
+    test = IrisDataset({'inputs': inputs[test_indices],
+            'labels': labels[test_indices]})
+
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size)
+
+    return train, test, train_loader, test_loader
