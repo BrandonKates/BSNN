82d35b1ff30e77e103a0cb83159a03f737c6e4ae
Brandon Kates <bjk224@cornell.edu>
2019-11-20 00:00:16 -0500

    fix model cuda issues

--- main.py
+++ main.py
@@ -7,21 +7,6 @@ from models import linear, bernoulli
 from parser import Parser
 from run_model import run_model
 
-def main():
-    args = Parser().parse()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
-    torch.manual_seed(1)
-    train_data, test_data, train_loader, test_loader = get_data(args)
-    print(train_data)
-    print("Train Data Shape: ", train_data.data.shape)
-    print("Test Data Shape: ", train_data.targets.shape)
-    # labels should be a whole number from [0, num_classes - 1]
-    output_size = int(max(max(train_data.targets), max(test_data.targets))) + 1
-    model =  construct_model(args, output_size)
-    criterion = nn.CrossEntropyLoss() #TODO : make generic
-    run_model(model, args, criterion, train_loader, test_loader, device)
-
 def get_data(args):
     if args.dataset == 'linear':
         train_data, test_data, train_loader, test_loader = linear_data.get(n=args.num_samples, d=args.input_size, sigma=0.15, test_split=0.2, batch_size=args.batch_size, num_workers=1)
@@ -40,6 +25,21 @@ def construct_model(args, output_size):
 
     elif args.model == "bernoulli":
         return bernoulli.BernoulliModel(args.input_size, [6 ,5, 6], output_size)
+
+def main():
+    args = Parser().parse()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    torch.manual_seed(1)
+    train_data, test_data, train_loader, test_loader = get_data(args)
+    print(train_data)
+    print("Train Data Shape: ", train_data.data.shape)
+    print("Test Data Shape: ", train_data.targets.shape)
+    # labels should be a whole number from [0, num_classes - 1]
+    output_size = int(max(max(train_data.targets), max(test_data.targets))) + 1
+    model =  construct_model(args, output_size).to(device)
+    criterion = nn.CrossEntropyLoss() #TODO : make generic
+    run_model(model, args, criterion, train_loader, test_loader, device)
 
 if __name__ == '__main__':
     main()
