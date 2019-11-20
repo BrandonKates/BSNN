811939dcad288af88f9c2b714ecc8046ed3ac6c5
BrandonKates <bjk224@cornell.edu>
2019-11-20 15:02:33 -0500

    added script to run mnist trials

---
+++ run_mnist_lecun.sh
@@ -0,0 +1,7 @@
+python main.py --dataset mnist --model bernoulli  --epochs 100 --input-size 784 --save-model --save-location "./checkpoints/mnist_bernoulli" --hidden-layers 300
+python main.py --dataset mnist --model bernoulli  --epochs 100 --input-size 784 --save-model --save-location "./checkpoints/mnist_bernoulli" --hidden-layers 1000
+
+python main.py --dataset mnist --model bernoulli  --epochs 1000 --input-size 784 --save-model --save-location "./checkpoints/mnist_bernoulli" --hidden-layers 300 100
+python main.py --dataset mnist --model bernoulli  --epochs 1000 --input-size 784 --save-model --save-location "./checkpoints/mnist_bernoulli" --hidden-layers 1000 150
+
+
