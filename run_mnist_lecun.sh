af0226a26477274bcfe8e308368eeb5f69f410b9
Kushal Babel <babel97kushal@gmail.com>
2019-11-20 15:52:04 -0500

    redirect and background

--- run_mnist_lecun.sh
+++ run_mnist_lecun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup python main.py --batch-size 64 --log-interval 100 --dataset mnist --model bernoulli  --epochs 1000 --input-size 784 --save-model --save-location "./checkpoints/mnist_bernoulli" --hidden-layers 300 > ./log/mnist/300.out 2> ./log/mnist/300.err & 
+python main.py --batch-size 64 --log-interval 1 --dataset mnist --model bernoulli  --epochs 1000 --input-size 784 --save-model --save-location "./checkpoints/mnist_bernoulli" --hidden-layers 300 > ./log/mnist/300.out 2> ./log/mnist/300.err &
 #nohup python main.py --batch-size 64 --log-interval 100 --dataset mnist --model bernoulli  --epochs 1000 --input-size 784 --save-model --save-location "./checkpoints/mnist_bernoulli" --hidden-layers 1000 > ./log/mnist/1000.out 2> ./log/mnist/1000.err &
 
 #nohup python main.py --batch-size 64 --log-interval 100 --dataset mnist --model bernoulli  --epochs 1000 --input-size 784 --save-model --save-location "./checkpoints/mnist_bernoulli" --hidden-layers 300 100 > ./log/mnist/300_100.out 2> ./log/mnist/300_100.err &
