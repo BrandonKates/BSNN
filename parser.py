1c22c9bbac52f258dd751c1491e6b51d9e95d7d2
Kushal Babel <babel97kushal@gmail.com>
2019-12-12 17:59:40 -0500

    make orthogonal defaul

--- parser.py
+++ parser.py
@@ -15,7 +15,7 @@ class Parser():
                                     help='SGD momentum')
                 self.parser.add_argument('--no-cuda', action='store_true', default=False,
                                     help='disables CUDA training')
-                self.parser.add_argument('--orthogonal', action='store_true', default=False,
+                self.parser.add_argument('--no-orthogonal', action='store_true', default=False,
                                     help='initialise orthogonal weight')
                 self.parser.add_argument('--seed', type=int, default=1,
                                     help='random seed')
