Gumbel Softmax code, nan losses, maybe some bug in the code

layers/bernoulli.py
@@ -20,7 +20,7 @@ class BernoulliLayer(nn.Module):
         self.squared_diff = []
 
     def forward(self, x, with_grad):
-        
+
         l = self.lin(x)
         with torch.no_grad():
             p = torch.sigmoid(l)

main.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import numpy as np
 
 from dataloaders import linear_data, xor_data, mnist_data, circle_data, spiral_data
-from models import linear, bernoulli
+from models import linear, bernoulli, gumbel
 from parser import Parser
 from run_model import run_model
 from math import log, ceil
@@ -35,6 +35,10 @@ def construct_model(args, output_size, num_labels, device='cpu'):
     elif args.model == "bernoulli":
         return bernoulli.BernoulliModel(args.input_size, hidden_layers, output_size, num_labels, device=device, orthogonal=not args.no_orthogonal)
 
+    elif args.model == "gumbel":
+        return gumbel.GumbelModel(args.input_size, hidden_layers, output_size, num_labels, device=device, orthogonal=not args.no_orthogonal)
+
+    
 def main():
     args = Parser().parse()
     use_cuda = not args.no_cuda and torch.cuda.is_available()

models/gumbel.py
@@ -1,4 +1,4 @@
-from layers import bernoulli
+from layers import gumbel
 
 import torch
 from torch import nn
@@ -8,11 +8,11 @@ import os
 import argparse
 from math import pow
 
-class BernoulliModel(nn.Module):
+class GumbelModel(nn.Module):
     def __init__(self, input_size, hidden_size_list, output_size, num_labels, device='cpu', orthogonal=True):
-        super(BernoulliModel, self).__init__()
+        super(GumbelModel, self).__init__()
         sizes = [input_size] + hidden_size_list
-        self.layers = nn.ModuleList([bernoulli.BernoulliLayer(sizes[i], sizes[i+1], device=device) for i in range(len(sizes)-1)])
+        self.layers = nn.ModuleList([gumbel.GumbelLayer(sizes[i], sizes[i+1], device=device) for i in range(len(sizes)-1)])
         self.linear_layer = nn.Linear(sizes[-1], output_size)
         if orthogonal:
             torch.nn.init.orthogonal_(self.linear_layer.weight)
@@ -25,9 +25,8 @@ class BernoulliModel(nn.Module):
         return self.linear_layer(x)
 
     def get_grad(self, losses):
-        for i in range(len(self.layers)):
-            self.layers[i].get_grad(losses)
- 
+        for loss in losses:
+            loss.backward()
 
     def predict(self, device, num_passes):
         def func(x):