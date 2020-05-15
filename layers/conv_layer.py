7a2e008c6851e3045d3c1ad66ad1fb2e82aebdfe
Bhargava Manja <bsm92@cornell.edu>
2020-05-15 12:04:11 -0400

    latest conv layer

--- layers/conv_layer.py
+++ layers/conv_layer.py
@@ -7,20 +7,24 @@ from copy import deepcopy
 from torch import exp, log
  
 class Conv2dLayer(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, device="cpu"):
+    def __init__(self, in_channels, out_channels, kernel_size, flatten=False, device="cpu"):
         super(Conv2dLayer, self).__init__()
         self.conv   = nn.Conv2d(in_channels, out_channels, kernel_size)
         self.device = device
+        self.flatten = flatten
 
 
     def forward(self, x, temp, with_grad):
         l = self.conv(x)
+        if self.flatten:
+            l = l.reshape((l.shape[0], l.shape[1]))
         # Change p to double so that gumbel_softmax func works
         delta = 1e-5
         p = torch.clamp(torch.sigmoid(l).double(), min=delta, max=1-delta)
         o = self.gumbel_softmax(p, temp)
         # Change output back to float for the next layer's input
         return o.float()
+
 
     def gumbel_softmax(self, p, temp):
         y1 = exp(( log(p) + self.sample_gumbel(p.shape) ) / temp)
