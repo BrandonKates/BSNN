7fa83eac7fc5786ff7136263395ce71319d10f2b
Bhargava Manja <bmanja@protonmail.com>
2020-06-02 18:23:54 -0400

    plot loss compares different logs

--- scripts/plot_loss.py
+++ scripts/plot_loss.py
@@ -1,30 +1,44 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import argparse
+import os
 
-def plot_log(log_file, out_file, display):
-    with open(log_file, 'r') as f:
-        losses = []
-        for line in f.readlines():
-            if 'Loss:' in line:
-                losses.append(float(line[line.find('Loss:') + len('Loss:'):line.find('\tTemp:')]))
+def plot_log(log_files, out_file, display, window_size):
+    for log_file in log_files:
+        with open(log_file, 'r') as f:
+            losses = []
+            for line in f.readlines():
+                if 'Loss:' in line:
+                    losses.append(float(line[line.find('Loss:') + len('Loss:'):line.find('\tTemp:')]))
 
-    plt.plot(losses)
+        if window_size > 1 and window_size <= len(losses):
+            ll = len(losses)
+            norm = 1/window_size
+            avg = sum(losses[:window_size])*norm
+            avgs = [avg]
+            for rem_i, add_i in zip(range(ll-window_size), range(window_size,ll)):
+                rem , add = losses[rem_i], losses[add_i]
+                avg += (norm * (add-rem))
+                avgs.append(avg)
 
+            losses = avgs
+
+        plt.plot(losses)
+
+    plt.legend(list(map(lambda f: os.path.basename(f), log_files)))
     if out_file:
         plt.savefig(out_file+'.png')
     if display:
-        plt.figure()
         plt.show()
 
 
 parser = argparse.ArgumentParser(description='Plot stuff')
 
-parser.add_argument('logfile', 
-        type=str, 
+parser.add_argument('logfiles', 
+        nargs='+', 
         help='path to log file')
 
-parser.add_argument('--save-image', '-s', 
+parser.add_argument('--output-image', '-o', 
         type=str,
         default='',
         help='filename if you want to save plot')
@@ -33,7 +47,11 @@ parser.add_argument('--no-display', '-n',
         action='store_true',
         help='provide if you want to only save plot')
 
+parser.add_argument('--smooth', '-s', 
+        type=int,
+        default=1,
+        help='window size to smooth output in')
+
 
 args = parser.parse_args()
-
-plot_log(args.logfile, args.save_image, display=not args.no_display)
+plot_log(args.logfiles,args.output_image,not args.no_display,args.smooth)
