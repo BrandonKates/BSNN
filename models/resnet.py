5491f8503c54e3db816a60ad747602467a328ec6
Bhargava Manja <bsm92@cornell.edu>
2020-07-13 15:20:29 -0400

    back to original stoch resnet

--- models/resnet.py
+++ models/resnet.py
@@ -21,26 +21,20 @@ class BasicBlock(nn.Module):
             groups=1, base_width=64, dilation=1):
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(in_planes, planes, device, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = conv3x3(planes, planes, device)
-        self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
-        self.ru1 = L.RU(planes, device)
-        self.ru2 = L.RU(planes, device)
+        self.nonlinearity = L.RU(planes, device)
 
 
     def forward(self, x):
         identity = x
         out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.ru1(out)
         out = self.conv2(out)
-        out = self.bn2(out)
         if self.downsample is not None:
             identity = self.downsample(x)
         out += identity
 
-        return self.ru2(out)
+        return self.nonlinearity(out)
 
 
 class ResNet(nn.Module):
@@ -54,8 +48,6 @@ class ResNet(nn.Module):
         self.groups = groups
         self.conv1 = L.Conv2d(3, self.in_planes, 7, device, True, stride=2,
                 padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(self.in_planes)
-        self.ru = L.RU(self.in_planes, device)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
@@ -64,7 +56,15 @@ class ResNet(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((1,1))
         self.fc = nn.Linear(512*block.expansion, 10)
         torch.nn.init.orthogonal_(self.fc.weight)
-        #self.fc.weight.requires_grad = False
+        self.fc.weight.requires_grad = False
+        '''
+        for m in self.modules():
+            if isinstance(m, L.Conv2d):
+                nn.init.kaiming_normal_(m.inner.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        '''
 
 
     def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
@@ -77,7 +77,7 @@ class ResNet(nn.Module):
             downsample = nn.Sequential(
                 conv1x1(self.in_planes, planes*block.expansion, self.device, 
                     stride=stride),
-                nn.BatchNorm2d(planes * block.expansion)
+                #nn.BatchNorm2d(planes * block.expansion)
             )
         
         layers = []
@@ -94,8 +94,6 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.ru(x)
         x = self.maxpool(x)
         x = self.layer1(x)
         x = self.layer2(x)
